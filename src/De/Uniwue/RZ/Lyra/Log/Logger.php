fd55fc5ce9fb57bb2f39a36c4e374469b185ce8d
Pouyan Azari <pouyan.azari@uni-wuerzburg.de>
2017-07-24 14:45:33 +0200

    add the subclass method to the top class

--- src/De/Uniwue/RZ/Lyra/Log/Logger.php
+++ src/De/Uniwue/RZ/Lyra/Log/Logger.php
@@ -137,4 +137,115 @@ class Logger{
     public function setName($name){
         $this->name = $name;
     }
+
+    /**
+     * Adds a log record at an arbitrary level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param mixed  $level   The log level
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function log($level, $message, array $context = [])
+    {
+        $this->getLogger()->log($level, $message, $context);
+    }
+
+    /**
+     * Adds a log record at the DEBUG level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function debug($message, array $context = [])
+    {
+        $this->getLogger()->debug($message, $context);
+    }
+    /**
+     * Adds a log record at the INFO level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function info($message, array $context = [])
+    {
+        $this->getLogger()->info($message, $context);
+    }
+    /**
+     * Adds a log record at the NOTICE level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function notice($message, array $context = [])
+    {
+        $this->getLogger()->notice($message, $context);
+    }
+    /**
+     * Adds a log record at the WARNING level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function warning($message, array $context = [])
+    {
+        $this->getLogger()->warning($message, $context);
+    }
+    /**
+     * Adds a log record at the ERROR level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function error($message, array $context = [])
+    {
+        $this->getLogger()->error($message, $context);
+    }
+    /**
+     * Adds a log record at the CRITICAL level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function critical($message, array $context = [])
+    {
+        $this->getLogger()->critical($message, $context);
+    }
+    /**
+     * Adds a log record at the ALERT level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function alert($message, array $context = [])
+    {
+        $this->getLogger()->alert($message, $context);
+    }
+    /**
+     * Adds a log record at the EMERGENCY level.
+     *
+     * This method allows for compatibility with common interfaces.
+     *
+     * @param string $message The log message
+     * @param array  $context The log context
+     */
+    public function emergency($message, array $context = [])
+    {
+        $this->getLogger()->emergency($message, $context);
+    }
 }
